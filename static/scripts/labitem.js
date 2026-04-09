c6afb07ed11f0c0fc21689c39eea208a10cab0fb
Alexey Slovetskikh <alexslcraft@gmail.com>
2026-04-10 00:05:20 +0300

    исправлена ошибка из-за которой в телеграмме не отображались некоторые классы

--- static/scripts/labitem.js
+++ static/scripts/labitem.js
@@ -9,30 +9,13 @@ function createLabItem(name, room, time) {
     const confBtn = document.createElement('button');
     const confBtnTxt = document.createElement('p');
 
-    labItemBg.classList.add(
-        'bg-base-medium-darkTheme',
-        'w-full',
-        'h-10',
-        'rounded-2',
-        'flex',
-        'items-center',
-        'mb-2',
-        'justify-between'
-    );
-
-    aboutLab.classList.add(
-        'flex',
-        'flex-col',
-        'items-start',
-        'w-14',
-        'ml-[15px]'
-    );
-
-    labName.classList.add('font-medium', 'text-white', 'text-[12px]');
-    labRoom.classList.add('font-light', 'text-white', 'text-[8px]', 'mt-[4px]');
-    labTime.classList.add('font-light', 'text-white', 'text-[8px]', 'mt-[2px]');
-    confBtn.classList.add('w-13', 'h-3', 'bg-primary-darkTheme', 'rounded-1', 'mr-[13px]', 'cursor-pointer', 'hover:bg-secondary-darkTheme', 'active:bg-[#00D390]', 'transition-color', 'duration-200');
-    confBtnTxt.classList.add('text-white', 'text-[12px]');
+    labItemBg.className = 'lab-item';
+    aboutLab.className = 'about-lab';
+    labName.className = 'lab-name';
+    labRoom.className = 'lab-room';
+    labTime.className = 'lab-time';
+    confBtn.className = 'conf-btn';
+    confBtnTxt.className = 'conf-btn-text';
 
     labName.textContent = name;
     labRoom.textContent = room;
