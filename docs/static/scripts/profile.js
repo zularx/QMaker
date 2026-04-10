46afa2cabd4874c8980a611e37d6bc2ea0001281
Alexey Slovetskikh <alexslcraft@gmail.com>
2026-04-10 17:41:29 +0300

    добавил отображение ID

--- docs/static/scripts/profile.js
+++ docs/static/scripts/profile.js
@@ -1,11 +1,14 @@
 const profileLink = document.getElementById('profileLink');
 const profilePicture = document.getElementById('profilePicture');
 const username = document.getElementById('username');
+const userId = document.getElementById('userId');
 
 const tg = window.Telegram?.WebApp;
 
 const user = tg?.initDataUnsafe?.user || {
-    first_name: "Guest"
+    first_name: "Guest",
+    photo_url: "static/images/unknownAvatar.png",
+    id: "@Guest"
 };
 
 if (tg) {
@@ -16,3 +19,4 @@ if (tg) {
 profileLink.textContent = user.first_name;
 profilePicture.setAttribute('src', user.photo_url);
 username.textContent = user.first_name;
+userId.textContent = user.id;
